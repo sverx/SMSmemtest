7b88ee92184612e6bb8d4df807e4c056eb23e230
sverx <sverx@yahoo.com>
2023-02-08 14:13:14 +0100

    Cartridge RAM tests added & make it compile fine with latest devkitSMS

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 SMSmemtest
 ==========
 
-SMSmemtest is a program for the SEGA Master System and SEGA Game Gear (in SMS mode) to test the perfect memory functionality.
+SMSmemtest is a program for the SEGA Master System and SEGA Game Gear (in SMS mode) to test the console's internal RAM and Video RAM memory and cartridge RAM memory, if present.
 
-It performs some basic tests on all the internal 8 KB system RAM and the 16 KB Video RAM, reporting results after that.
+It performs some basic tests on all the internal 8 KiB system RAM, on the 16 KiB Video RAM and also on the (up to 32KiB) cartridge RAM reporting results after that.
 
-This is work-in-progress, more advanced tests will be added.
+Can be compiled using latest devkitSMS with:
+
+
+```
+# compile the source:
+sdcc -c -mz80 --peep-file peep-rules.txt main.c
+
+# link with devkitSMS' SMSlib and crt0 module
+sdcc -o SMSmemtest.ihx -mz80 --no-std-crt0 --data-loc 0xC000 ../crt0/crt0_sms.rel ../SMSlib/SMSlib.lib main.rel
+
+# create final ROM (out will create a 32 KiB ROM - if you need a 64 KiB ROM please run 'ihx2sms -pm SMSmemtest.ihx SMSmemtest.sms' instead)
+ihx2sms SMSmemtest.ihx SMSmemtest.sms
+```
 
--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
 
 #define MAJ_VERSION  0
-#define MIN_VERSION  20
+#define MIN_VERSION  30
 
 #include <stdio.h>
 #include "../SMSlib/SMSlib.h"
+
+extern volatile unsigned int KeysStatus,PreviousKeysStatus;
+extern unsigned char VDPReg[2];
 
 /* ********************************************************************* */
 #pragma save
@@ -181,7 +184,7 @@ _check_failed_vram:
 }
 #pragma restore
 
-unsigned char vram_test_2 (void) __naked {
+unsigned char vram_test_2 (void) __z88dk_fastcall __naked {
   __asm
     ld hl,#0x4000      ; start of VRAM (write)
     rst #0x08          ; write VDP control port
@@ -233,6 +236,129 @@ _check_failed_vram_2:
 }
 
 /* ********************************************************************* */
+
+#pragma save
+#pragma disable_warning 85
+unsigned char sram_test (unsigned char value) __z88dk_fastcall __naked {
+  __asm
+    di
+    pop de             ; get ret address from stack
+    ld a,l             ; get value to write and read
+    ld hl,#0x8000      ; start of SRAM
+    ld bc,#0x0040      ; 64 times 256 bytes = 16 KB
+
+_write_loop_3:
+    ld (hl),a
+    inc hl
+    djnz _write_loop_3
+
+    dec c
+    jr nz,_write_loop_3
+
+// put a long delay after the write, so that if any bit changes we can get it
+
+    ld bc,#0x0000      ; longest delay I can imagine ;)
+_delay_loop_3:
+    .db #0xFD
+    ld l,a             ; ld iyl,a since this is a pretty slow operation
+    .db #0xFD
+    ld l,a             ; ld iyl,a since this is a pretty slow operation
+    .db #0xFD
+    ld l,a             ; ld iyl,a since this is a pretty slow operation
+
+    djnz _delay_loop_3
+    dec c
+    jr nz,_delay_loop_3
+
+    .db #0xFD
+    ld l,a             ; save written value in iyl
+    ld hl,#0x8000      ; start of SRAM
+    ld bc,#0x0040      ; 64 times 256 bytes = 16 KB
+
+_read_loop_3:
+    ld a,(hl)
+    .db #0xFD
+    cp l               ; compare with value in iyl
+    jr nz,_check_failed_3
+    inc hl
+    djnz _read_loop_3
+
+    dec c
+    jr nz,_read_loop_3
+
+    ld l,#0
+    push de            ; restore ret address into stack
+    ei
+    ret
+
+_check_failed_3:
+    ld l,#1
+    push de            ; restore ret address into stack
+    ei
+    ret
+  __endasm;
+}
+#pragma restore
+
+unsigned char sram_test_2 (void) __z88dk_fastcall __naked {
+  __asm
+    di
+    pop de             ; get ret address from stack
+    ld a,l             ; get value to write and read
+    ld hl,#0x8000      ; start of SRAM
+    ld bc,#0x0040      ; 64 times 256 bytes = 16 KB
+
+_write_loop_4:
+    ld (hl),l
+    inc hl
+    djnz _write_loop_4
+
+    dec c
+    jr nz,_write_loop_4
+
+// put a long delay after the write, so that if any bit changes we can get it
+
+    ld bc,#0x0000      ; longest delay I can imagine ;)
+_delay_loop_4:
+    .db #0xFD
+    ld l,a             ; ld iyl,a since this is a pretty slow operation
+    .db #0xFD
+    ld l,a             ; ld iyl,a since this is a pretty slow operation
+    .db #0xFD
+    ld l,a             ; ld iyl,a since this is a pretty slow operation
+
+    djnz _delay_loop_4
+    dec c
+    jr nz,_delay_loop_4
+
+    ld hl,#0x8000      ; start of SRAM
+    ld bc,#0x0040      ; 64 times 256 bytes = 16 KB
+
+_read_loop_4:
+    ld a,(hl)
+    cp l               ; compare with value in l
+    jr nz,_check_failed_4
+    inc hl
+    djnz _read_loop_4
+
+    dec c
+    jr nz,_read_loop_4
+
+    ld l,#0
+    push de            ; restore ret address into stack
+    ei
+    ret
+
+_check_failed_4:
+    ld l,#1
+    push de            ; restore ret address into stack
+    ei
+    ret
+  __endasm;
+}
+
+/* ********************************************************************* */
+
 void print_test_result (unsigned char failed) {
   if (failed) {
     SMS_configureTextRenderer(-32+96*2);    // set text to RED
@@ -261,6 +387,7 @@ void init_display (void) {
   SMS_displayOn();
   SMS_setNextTileatXY (1,1);
   printf ("sverx's SMS memtest v%d.%02d",MAJ_VERSION,MIN_VERSION);
+  SMS_VDPturnOffFeature(VDPFEATURE_FRAMEIRQ);
 }
 
 void init_display_vram_tests (void) {
@@ -310,12 +437,20 @@ void main (void) {
   SMS_setNextTileatXY (1,14);
   printf ("(screen will be off a few secs)");
 
+  KeysStatus=0;
+  PreviousKeysStatus=0;
+  VDPReg[0]=0x04;
+  VDPReg[1]=0x20;
+  SMS_setFrameInterruptHandler(0);
+  SMS_displayOn();
+  SMS_VDPturnOnFeature(VDPFEATURE_FRAMEIRQ);
+
   while (!(SMS_getKeysPressed() & (PORT_A_KEY_UP|PORT_A_KEY_DOWN|PORT_A_KEY_LEFT|PORT_A_KEY_RIGHT|PORT_A_KEY_1|PORT_A_KEY_2|
                                    PORT_B_KEY_UP|PORT_B_KEY_DOWN|PORT_B_KEY_LEFT|PORT_B_KEY_RIGHT|PORT_B_KEY_1|PORT_B_KEY_2)));    // wait for any key press
 
-
 /* *** VIDEO RAM tests ************************************************* */
 
+  SMS_VDPturnOffFeature(VDPFEATURE_FRAMEIRQ);
   SMS_displayOff();
 
   if (vram_test(0x00)) {                             // notify if this test failed
@@ -348,7 +483,84 @@ void main (void) {
 
     SMS_setNextTileatXY (1,10);
     printf ("VIDEO RAM tests completed");
+
+    SMS_setNextTileatXY (1,12);
+    printf ("Press any controller button");
+    SMS_setNextTileatXY (1,13);
+    printf ("to start cart SRAM tests");
   }
+
+  KeysStatus=0;
+  PreviousKeysStatus=0;
+  SMS_setFrameInterruptHandler(0);
+  SMS_VDPturnOnFeature(VDPFEATURE_FRAMEIRQ);
+
+  while (!(SMS_getKeysPressed() & (PORT_A_KEY_UP|PORT_A_KEY_DOWN|PORT_A_KEY_LEFT|PORT_A_KEY_RIGHT|PORT_A_KEY_1|PORT_A_KEY_2|
+                                   PORT_B_KEY_UP|PORT_B_KEY_DOWN|PORT_B_KEY_LEFT|PORT_B_KEY_RIGHT|PORT_B_KEY_1|PORT_B_KEY_2)));    // wait for any key press
+
+
+/* *** SRAM tests ******************************************************* */
+
+  init_display();
+
+  SMS_VDPturnOffFeature(VDPFEATURE_FRAMEIRQ);
+
+  SMS_enableSRAMBank(0);  // activate SRAM bank 0
+
+  SMS_setNextTileatXY (1,3);
+  printf ("Testing cart SRAM (bank 0):");
+  SMS_setNextTileatXY (1,4);
+  printf ("-all bits to zero...");
+  print_test_result (sram_test (0x00));
+
+  SMS_setNextTileatXY (1,5);
+  printf ("-all bits to one ...");
+  print_test_result (sram_test (0xFF));
+
+  SMS_setNextTileatXY (1,6);
+  printf ("-all bytes to $55...");
+  print_test_result (sram_test (0x55));
+
+  SMS_setNextTileatXY (1,7);
+  printf ("-all bytes to $AA...");
+  print_test_result (sram_test (0xAA));
+
+  SMS_setNextTileatXY (1,8);
+  printf ("-different values...");
+  print_test_result (sram_test_2 ());
+
+  // more tests here!
+
+  SMS_enableSRAMBank(1);  // activate SRAM bank 1
+
+  SMS_setNextTileatXY (1,10);
+  printf ("Testing cart SRAM (bank 1):");
+  SMS_setNextTileatXY (1,11);
+  printf ("-all bits to zero...");
+  print_test_result (sram_test (0x00));
+
+  SMS_setNextTileatXY (1,12);
+  printf ("-all bits to one ...");
+  print_test_result (sram_test (0xFF));
+
+  SMS_setNextTileatXY (1,13);
+  printf ("-all bytes to $55...");
+  print_test_result (sram_test (0x55));
+
+  SMS_setNextTileatXY (1,14);
+  printf ("-all bytes to $AA...");
+  print_test_result (sram_test (0xAA));
+
+  SMS_setNextTileatXY (1,15);
+  printf ("-different values...");
+  print_test_result (sram_test_2 ());
+
+  // more tests here!
+
+  SMS_disableSRAM(); // deactivate SRAM
+
+  SMS_setNextTileatXY (1,17);
+  printf ("Cart SRAM tests completed");
 
   for (;;);
 
